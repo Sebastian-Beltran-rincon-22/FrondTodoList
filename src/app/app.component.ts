17dbcba3ab1a73891259cd182a23eab71d5e7ee9
nicolas <jaljordan77@gmail.com>
2023-08-01 16:24:52 -0500

    update->src/app/app.component.ts= date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Item } from './item';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,14 +9,16 @@ import { Item } from './item';
 export class AppComponent {
   title = 'todo';
 
+  currentDate: Date = new Date();
+
   //ToDo: Eliminar esta lista al momento de integrar la API
   filter: "all" | "active" | "done" = "all"
 
+
+
   allItems = [
-    {description: "dormir",done:true},
-
+    {description: "dormir",done:true,date:"20/03/2023"},
   ]
-
   get items() {
     if (this.filter === "all"){
       return this.allItems
@@ -25,21 +28,25 @@ export class AppComponent {
     })
   }
 
-  addItem(description:string){
+  addItem(description:string,date:any){
     this.allItems.unshift({
       description,
-      done:false
+      done:false,
+      date:date
     })
+    this.currentDate = new Date();
+
   }
 
   removeItem(item:Item){
     this.allItems.splice(this.allItems.indexOf(item),1)
   }
 
-  updateItem(newvalue:string,oldtvalue:string): void {
+  updateItem(newvalue:string,oldtvalue:string,newdate:string): void {
     const index = this.allItems.findIndex((item) => item.description === oldtvalue);
     if (index !== -1) {
-      this.allItems[index] = {description:newvalue,done:false};
+      this.allItems[index] = {description:newvalue,done:false,date:newdate};
+
       }
     }
 }
