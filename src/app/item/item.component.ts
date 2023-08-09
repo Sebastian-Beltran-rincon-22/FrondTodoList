62c55cbb90ecab878a47b290e5e4efaa53a336e7
sewastian <sebastianrincon834@gmail.com>
2023-08-08 21:16:58 -0500

    update [item -> item.component.ts]

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Item } from '../shared/models/item';
+import { TodoService } from '../core/service/todo.service';
 
 @Component({
   selector: 'app-item',
@@ -7,4 +9,31 @@ import { Component } from '@angular/core';
 })
 export class ItemComponent {
 
+  constructor(private TodoService: TodoService){
+
+  }
+  @Input() Item!: Item
+
+
+  updateItem(title: string, desc: string, responsable: string, date: string, _id:string):void {
+    const updatedTask ={
+      todo: title,
+      description: desc,
+      responsible: responsable,
+      dueData: date,
+      done: false,
+    }
+    this.TodoService.updateTask(`https://todolist-vp6o.onrender.com/todo/update/${_id}`, updatedTask)
+    .subscribe((data:any) => {
+      console.log(data)
+      this.Item = data
+    });
+  }
+
+  deleteItem(id: string) {
+    this.TodoService.deleteTask(`https://todolist-vp6o.onrender.com/todo/delete/${id}`)
+    .subscribe((response) =>{
+      console.log(response)
+    });
+  }
 }
